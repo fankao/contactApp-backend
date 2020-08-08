5b266f76a014f442d07b94d8320c2f6aaf09c08c
chien <minhchiendc@gmail.com>
2020-08-08 16:02:54 +0700

    Update crossOrigin

--- src/main/java/com/contact/api/ContactApi.java
+++ src/main/java/com/contact/api/ContactApi.java
@@ -1,6 +1,5 @@
 package com.contact.api;
 
-import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.contact.dao.ContactRepository;
 import com.contact.entities.Contact;
 
 @RestController
 @RequestMapping("/api/contacts")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://contact-angular-frontend.herokuapp.com")
 public class ContactApi {
 	@Autowired
 	private ContactRepository service;
@@ -33,30 +31,30 @@ public class ContactApi {
 	@GetMapping()
 	public ResponseEntity<Page<Contact>> getContacts(@RequestParam(name = "page", defaultValue = "0") int page,
 			@RequestParam(value = "size", defaultValue = "10") int size) {
-		return ResponseEntity.ok(service.findAll(PageRequest.of(page, size,Sort.by("lastUpdated").descending())));
+		return ResponseEntity.ok(service.findAll(PageRequest.of(page, size, Sort.by("lastUpdated").descending())));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Contact> getContactById(@PathVariable("id")Long id) {
+	public ResponseEntity<Contact> getContactById(@PathVariable("id") Long id) {
 		Optional<Contact> findedContact = service.findById(id);
-		if(findedContact.isPresent()) {
+		if (findedContact.isPresent()) {
 			return ResponseEntity.ok(findedContact.get());
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
-	
-	@RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT})
+
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
 	public ResponseEntity<Contact> saveContact(@RequestBody Contact contact) {
-		System.out.println("Contact info: "+contact);
+		System.out.println("Contact info: " + contact);
 		service.save(contact);
 		// Tạo URI lưu thông tin cho todo
-		//URI location = ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(contact.getId()).toUri();
-		return new ResponseEntity<Contact>(contact,HttpStatus.CREATED);
+		// URI location =
+		// ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(contact.getId()).toUri();
+		return new ResponseEntity<Contact>(contact, HttpStatus.CREATED);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Contact> deleteContact(@PathVariable("id")Long id) {
+	public ResponseEntity<Contact> deleteContact(@PathVariable("id") Long id) {
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
